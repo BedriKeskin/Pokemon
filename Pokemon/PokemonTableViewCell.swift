39298caa306b36b58c06a2fc3000230d0134f233
bedri.keskin <bedri.keskin@gmail.com>
2024-01-17 02:47:21 +0300

    nice

---
+++ Pokemon/PokemonTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PokemonTableViewCell.swift
+//  Pokemon
+//
+//  Created by Bedri Keskin on 17.01.2024.
+//
+
+import UIKit
+
+class PokemonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
