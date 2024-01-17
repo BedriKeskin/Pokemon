c5a984a703168bbd5dcc20328048daaa9c776c7b
bedri.keskin <bedri.keskin@gmail.com>
2024-01-18 01:09:01 +0300

    very nice

--- Pokemon/ViewController.swift
+++ Pokemon/ViewController.swift
@@ -10,10 +10,25 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+           super.viewDidLoad()
 
+        print("xxxx")
 
+           if let productView = Bundle.main.loadNibNamed("\(PokemonTableViewCell.self)", owner: self, options: nil)?.first as? PokemonTableViewCell {
+               self.view.addSubview(productView)
+
+               productView.translatesAutoresizingMaskIntoConstraints = false
+               //productView.Picture.image = UIImage(named: "iPhone 11")
+               productView.Name.text = "iPhone 11"
+               productView.Info.text = "Kullanım konusunda hiçbir karışıklığa yer vermeden tonlarca yetenekle donatılmış çığır açıcı üçlü kamera sistemi. Pil ömrü konusunda eşi benzeri görülmemiş büyüklükte bir adım."
+
+               NSLayoutConstraint.activate([
+                   productView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                   productView.centerYAnchor.constraint(equalTo: view.topAnchor, constant: 200),
+                   productView.widthAnchor.constraint(equalToConstant: 414),
+                   productView.heightAnchor.constraint(equalToConstant: 240)
+               ])
+           }
+       }
 }
 
